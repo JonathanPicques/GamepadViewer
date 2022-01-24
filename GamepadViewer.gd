684d0f5c103ae43aab5e22e185645db3bf18f015
JonathanPicques <jonathan.picques@gmail.com>
2022-01-24 15:09:52 +0100

    Add background color

--- GamepadViewer.gd
+++ GamepadViewer.gd
@@ -17,7 +17,14 @@ const CONTEXT_MENU_GAMEPAD_DEVICE_1 := "Gamepad #1"
 const CONTEXT_MENU_GAMEPAD_DEVICE_2 := "Gamepad #2"
 const CONTEXT_MENU_GAMEPAD_DEVICE_3 := "Gamepad #3"
 const CONTEXT_MENU_GAMEPAD_DEVICE_4 := "Gamepad #4"
+const CONTEXT_MENU_BACKGROUND_COLOR_WHITE := "White"
+const CONTEXT_MENU_BACKGROUND_COLOR_MAGENTA := "Magenta"
+const CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT := "Transparent"
 
+const CONTEXT_MENU_DEVICES := [CONTEXT_MENU_GAMEPAD_DEVICE_1, CONTEXT_MENU_GAMEPAD_DEVICE_2, CONTEXT_MENU_GAMEPAD_DEVICE_3, CONTEXT_MENU_GAMEPAD_DEVICE_4]
+const CONTEXT_MENU_BACKGROUND_COLORS := [CONTEXT_MENU_BACKGROUND_COLOR_WHITE, CONTEXT_MENU_BACKGROUND_COLOR_MAGENTA, CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT]
+
+onready var Background: ColorRect = $Background
 onready var ConfigContextMenu: PopupMenu = $ConfigContextMenu
 
 # @impure
@@ -31,16 +38,21 @@ func _ready():
 	ConfigContextMenu.add_separator("Gamepad")
 	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_GAMEPAD_PS4)
 	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_GAMEPAD_XBOX)
-	ConfigContextMenu.add_separator()
+	ConfigContextMenu.add_separator("Device")
 	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_GAMEPAD_DEVICE_1)
 	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_GAMEPAD_DEVICE_2)
 	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_GAMEPAD_DEVICE_3)
 	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_GAMEPAD_DEVICE_4)
+	ConfigContextMenu.add_separator("Background")
+	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_BACKGROUND_COLOR_MAGENTA)
+	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_BACKGROUND_COLOR_WHITE)
+	ConfigContextMenu.add_radio_check_item(CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT)
 	ConfigContextMenu.add_separator()
 	ConfigContextMenu.add_item(CONTEXT_MENU_QUIT)
 	# setup "Gamepad #1" as the default gamepad device.
 	_enable_gamepad(CONTEXT_MENU_GAMEPAD_PS4)
 	_enable_gamepad_device(CONTEXT_MENU_GAMEPAD_DEVICE_1, 0)
+	_enable_gamepad_background(CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT)
 
 # @impure
 func _input(event: InputEvent):
@@ -87,10 +99,30 @@ func _enable_gamepad_device(menu_text: String, _gamepad_device: int):
 	for item_index in ConfigContextMenu.get_item_count():
 		var item_id := ConfigContextMenu.get_item_id(item_index)
 		var item_text := ConfigContextMenu.get_item_text(item_index)
-		if item_text.begins_with("Gamepad #"):
+		if CONTEXT_MENU_DEVICES.has(item_text):
 			ConfigContextMenu.set_item_checked(item_id, item_text == menu_text)
 	# assign gamepad device.
 	gamepad_device = _gamepad_device
+
+# @impure
+func _enable_gamepad_background(menu_text: String):
+	# check or uncheck in the context menu.
+	for item_index in ConfigContextMenu.get_item_count():
+		var item_id := ConfigContextMenu.get_item_id(item_index)
+		var item_text := ConfigContextMenu.get_item_text(item_index)
+		if CONTEXT_MENU_BACKGROUND_COLORS.has(item_text):
+			ConfigContextMenu.set_item_checked(item_id, item_text == menu_text)
+	# assign gamepad background color.
+	match menu_text:
+		CONTEXT_MENU_BACKGROUND_COLOR_WHITE:
+			Background.color = Color.white
+			Background.visible = true
+		CONTEXT_MENU_BACKGROUND_COLOR_MAGENTA:
+			Background.color = Color.magenta
+			Background.visible = true
+		CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT:
+			Background.color = Color.transparent
+			Background.visible = false
 
 # @impure
 func _on_config_context_menu_id_pressed(id: int):
@@ -102,6 +134,9 @@ func _on_config_context_menu_id_pressed(id: int):
 		CONTEXT_MENU_GAMEPAD_DEVICE_2: _enable_gamepad_device(CONTEXT_MENU_GAMEPAD_DEVICE_2, 1)
 		CONTEXT_MENU_GAMEPAD_DEVICE_3: _enable_gamepad_device(CONTEXT_MENU_GAMEPAD_DEVICE_3, 2)
 		CONTEXT_MENU_GAMEPAD_DEVICE_4: _enable_gamepad_device(CONTEXT_MENU_GAMEPAD_DEVICE_4, 3)
+		CONTEXT_MENU_BACKGROUND_COLOR_MAGENTA: _enable_gamepad_background(CONTEXT_MENU_BACKGROUND_COLOR_MAGENTA)
+		CONTEXT_MENU_BACKGROUND_COLOR_WHITE: _enable_gamepad_background(CONTEXT_MENU_BACKGROUND_COLOR_WHITE)
+		CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT: _enable_gamepad_background(CONTEXT_MENU_BACKGROUND_COLOR_TRANSPARENT)
 
 ##
 # Gamepad helpers
